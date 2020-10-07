128b84d8962a03fc091802624e65d36359b36419
Christopher McDougal <cmcdougal@Flexcare.local>
2020-10-07 09:25:38 -0700

    Added 'index.html' 'script.js' and 'styles.css'.

---
+++ script.js
@@ -0,0 +1,29 @@
+function populateList(v1)
+{
+    var ul = document.getElementById("dynamic-list");
+    var pr = v1
+    var li = document.createElement("li");
+    
+    
+
+    foreach(e in v1)
+    {
+        li.setAttribute('id', pr.value);
+        li.appendChild(document.createTextNode(e.value));
+        ul.appendChild(li);
+    }
+
+}
+
+function getRepos(url)
+{
+    httpReq = new XMLHttpRequest();
+    httpReq.open("GET", url, false);
+    res = httpReq.send() 
+    return res;
+}
+
+window.onload = function () {
+    var v = getRepos("https://github.com/Nightsmore?tab=repositories")
+    populateList(v);
+}
