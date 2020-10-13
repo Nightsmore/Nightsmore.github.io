94659714acd50707b26fa12e682b88a669ee4c5c
Christopher McDougal <cmcdougal@Flexcare.local>
2020-10-13 16:19:39 -0700

    slight changes

--- script.js
+++ script.js
@@ -1,29 +1,49 @@
-function populateList(v1)
+function populateList(pr)
 {
     var ul = document.getElementById("dynamic-list");
-    var pr = v1
+    
     var li = document.createElement("li");
     
     
-
-    foreach(e in v1)
+    for(i = 0; i < pr.length; i++)
     {
         li.setAttribute('id', pr.value);
-        li.appendChild(document.createTextNode(e.value));
+        li.appendChild(document.createTextNode(pr[i].value));
         ul.appendChild(li);
     }
+    
 
 }
 
-function getRepos(url)
+/*
+function getRepos(url, callback)
 {
     httpReq = new XMLHttpRequest();
-    httpReq.open("GET", url, false);
-    res = httpReq.send() 
-    return res;
+    httpReq.onreadystatechange = function() {
+        if(httpReq.readyState == 4 && httpReq.status == 200) 
+            callback(httpReq.responseText); 
+    }
+    
+    httpReq.open("GET", url, true);
+    httpReq.send(null);
+}
+*/
+function httpGet(url)
+{
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var myArr = JSON.parse(this.responseText);
+            populateList(myArr);
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+    
 }
 
 window.onload = function () {
-    var v = getRepos("https://api.github.com/users/Nightsmore/repos")
-    populateList(v);
+    httpGet("https://api.github.com/users/Nightsmore/repos")
+    
+    
 }
